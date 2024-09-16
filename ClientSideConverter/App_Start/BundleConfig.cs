5484f3659b2ed8a48492def3c4a859d4d068d04f
Daniel Vinther Andersen <dva@inropa.com>
2024-09-16 12:53:04 +0200

    ClientSideConvert JQuery

---
+++ ClientSideConverter/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ClientSideConverter
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/BootstrapCSS").Include(
+                      "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Main/MainCSS").Include(
+                "~/Content/Main/MainCSS.min.css"));
+        }
+    }
+}
---
+++ ClientSideConverter/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientSideConverter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Distance()
+        {
+            return View();
+        }
+
+        public ActionResult Light()
+        {
+            return View();
+        }
+
+        public ActionResult Power()
+        {
+            return View();
+        }
+
+        public ActionResult Pressure()
+        {
+            return View();
+        }
+
+        public ActionResult Speed()
+        {
+            return View();
+        }
+
+        public ActionResult Temperature()
+        {
+            return View();
+        }
+
+        public ActionResult Time()
+        {
+            return View();
+        }
+
+        public ActionResult Torque()
+        {
+            return View();
+        }
+    }
+}
