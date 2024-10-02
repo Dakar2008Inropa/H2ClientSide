3747ac09d027527586a92407b1aff36a8a67e55e
Daniel Vinther Andersen <dva@inropa.com>
2024-10-02 07:46:43 +0200

    Added Students WebAPI Site

---
+++ ClientSideStudentAPI/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ClientSideStudentAPI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Library/JS/JQuery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Library/JS/JQueryValidate/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Library/JS/Modernizr/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Library/JS/Bootstrap/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Library/CSS/BootstrapCSS").Include(
+                      "~/Library/CSS/Bootstrap/bootstrap.min.css"));
+        }
+    }
+}
---
+++ ClientSideStudentAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClientSideStudentAPI.MvcApplication" Language="C#" %>
